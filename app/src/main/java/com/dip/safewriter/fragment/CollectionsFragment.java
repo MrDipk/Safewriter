271f3f8b2dc8293272e8ef5b374563af6fdfcbbb
MrDipk <deepakchouhan8875@gmail.com>
2021-01-13 16:46:08 +0530

    new project with api implementation

---
+++ app/src/main/java/com/dip/safewriter/fragment/CollectionsFragment.java
@@ -0,0 +1,18 @@
+package com.dip.safewriter.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.dip.safewriter.R;
+
+public class CollectionsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_collections, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/dip/safewriter/fragment/PhotosFragment.java
@@ -0,0 +1,19 @@
+package com.dip.safewriter.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.dip.safewriter.R;
+
+public class PhotosFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        return root;
+    }
+}
